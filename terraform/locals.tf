6b638e724e85070ae29bb16835015ccf976887a4
Daniel M <daniel.maruani@gmail.com>
2024-07-20 10:12:37 -0400

    remove webhooks

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -8,7 +8,8 @@ locals {
   }
 
   branch_protections = {
-    for repository_key, repository in var.repositories : repository_key => repository if repository.enable_branch_protection
+    for repository_key, repository in var.repositories : repository_key => repository
+    if repository.enable_branch_protection
   }
 
   child_team_repositories = {
@@ -21,15 +22,9 @@ locals {
           permission = team_child.permission
         }
       ]
-  ]) : "${repository.team_child}-${repository.repository}" => repository }
-
-  datadog_webhooks = {
-    for repository_key, repository in var.repositories : repository_key => repository if repository.enable_datadog_webhook
+    ]) : "${repository.team_child}-${repository.repository}" => repository
   }
 
-  discord_webhooks = {
-    for repository_key, repository in var.repositories : repository_key => repository if repository.enable_discord_webhook
-  }
 
   members = {
     for user in var.members : user => "member"
@@ -45,10 +40,12 @@ locals {
           permission  = team_parent.permission
         }
       ]
-  ]) : "${repository.team_parent}-${repository.repository}" => repository }
+    ]) : "${repository.team_parent}-${repository.repository}" => repository
+  }
 
   review_request_delegations = {
-    for team_parent_key, team_parent in var.team_parents : team_parent_key => team_parent if team_parent.review_request_delegation
+    for team_parent_key, team_parent in var.team_parents : team_parent_key => team_parent
+    if team_parent.review_request_delegation
   }
 
   users = merge(local.admins, local.members)
