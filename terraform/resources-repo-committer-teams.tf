e74ad28a1f0456433810abb23c147e712ddf04cd
Daniel M <daniel.maruani@gmail.com>
2024-08-03 17:10:59 -0400

    fix tf

--- terraform/resources-repo-committer-teams.tf
+++ terraform/resources-repo-committer-teams.tf
@@ -19,7 +19,7 @@ resource "github_team_members" "repo_committer_team_members" {
 
     content {
       username = members.value
-      role     = contains(var.admins, members.value) ? "maintainer" : "member"
+      role     = contains(each.value.admins, members.value) ? "maintainer" : "member"
     }
   }
 }
