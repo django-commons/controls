152fcd4d660b6837cffc1e3ba52ccb79baf53882
Daniel M <daniel.maruani@gmail.com>
2024-09-02 17:36:20 -0600

    feat: create envs using terraform

--- terraform/resources-environments.tf
+++ terraform/resources-environments.tf
@@ -1,41 +1,18 @@
-import {
-  for_each = { for k, v in var.repositories : k => v if v.is_django_commons_repo == false }
-
-  id = "${each.key}:pypi"
-  to = github_repository_environment.pypi[each.key]
-}
-
 resource "github_repository_environment" "pypi" {
-  for_each = { for k, v in var.repositories : k => v if v.is_django_commons_repo == false }
+  for_each = {for k, v in var.repositories : k => v if v.is_django_commons_repo == false}
 
   environment         = "pypi"
   repository          = each.key
-  prevent_self_review = true
+  prevent_self_review = false
   reviewers {
     teams = [github_team.repo_admin_team[each.key].id]
   }
-  deployment_branch_policy {
-    protected_branches     = false
-    custom_branch_policies = false
-  }
-}
-
-import {
-  for_each = { for k, v in var.repositories : k => v if v.is_django_commons_repo == false }
-
-  id = "${each.key}:testpypi"
-  to = github_repository_environment.testpypi[each.key]
 }
 
 resource "github_repository_environment" "testpypi" {
-  for_each = { for k, v in var.repositories : k => v if v.is_django_commons_repo == false }
+  for_each = {for k, v in var.repositories : k => v if v.is_django_commons_repo == false}
 
   environment         = "testpypi"
   repository          = each.key
-  prevent_self_review = true
-
-  deployment_branch_policy {
-    protected_branches     = false
-    custom_branch_policies = false
-  }
+  prevent_self_review = false
 }
