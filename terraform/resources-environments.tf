57b93e988c4c0a1d327b9bc7d15d03146583cfcf
cunla <cunla@users.noreply.github.com>
2024-09-02 23:28:25 +0000

    [AUTO]Format code
    Files changed:
    M	terraform/resources-environments.tf

--- terraform/resources-environments.tf
+++ terraform/resources-environments.tf
@@ -1,12 +1,12 @@
 import {
-  for_each = {for k, v in var.repositories : k => v if v.is_django_commons_repo == false}
+  for_each = { for k, v in var.repositories : k => v if v.is_django_commons_repo == false }
 
   id = "${each.key}:pypi"
   to = github_repository_environment.pypi[each.key]
 }
 
 resource "github_repository_environment" "pypi" {
-  for_each = {for k, v in var.repositories : k => v if v.is_django_commons_repo == false}
+  for_each = { for k, v in var.repositories : k => v if v.is_django_commons_repo == false }
 
   environment         = "pypi"
   repository          = each.key
@@ -21,14 +21,14 @@ resource "github_repository_environment" "pypi" {
 }
 
 import {
-  for_each = {for k, v in var.repositories : k => v if v.is_django_commons_repo == false}
+  for_each = { for k, v in var.repositories : k => v if v.is_django_commons_repo == false }
 
   id = "${each.key}:testpypi"
   to = github_repository_environment.testpypi[each.key]
 }
 
 resource "github_repository_environment" "testpypi" {
-  for_each = {for k, v in var.repositories : k => v if v.is_django_commons_repo == false}
+  for_each = { for k, v in var.repositories : k => v if v.is_django_commons_repo == false }
 
   environment         = "testpypi"
   repository          = each.key
